c5c93b8bbab6d770dcb7629b9d3913b44d451ce8
Bo Yang <rintaigyoku@126.com>
2020-03-19 19:11:46 +0800

    Add some code to test how to use this library

---
+++ test/TestSerialPortStream/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RJCP.IO.Ports;
+using System.Threading;
+
+namespace TestSerialPortStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test();
+        }
+        static void Test()
+        {
+            SerialPortStream serialPortStream = new SerialPortStream("COM1", 115200, 8, Parity.None, StopBits.One);
+            serialPortStream.DataReceived += new EventHandler<SerialDataReceivedEventArgs>(DataReceived);
+            serialPortStream.Open();
+            if(serialPortStream.IsOpen)
+            {
+                serialPortStream.WriteLine("This is a test");
+                serialPortStream.Flush();
+            }
+
+            while(true)
+            {
+                Thread.Sleep(1000);
+                if(serialPortStream.CanRead)
+                {
+                    var all = serialPortStream.ReadExisting();
+                    if(!string.IsNullOrEmpty(all))
+                        Console.WriteLine(all);
+                }
+            }
+        }
+        static void DataReceived(object sender, SerialDataReceivedEventArgs args)
+        {
+            if(args != null)
+            {
+                if(sender is SerialPortStream sps)
+                {
+                    var all = sps.ReadExisting();
+                    Console.WriteLine(all);
+                }
+            }
+        }
+    }
+}
